334c8928dc4a7d8eb0b6d709159f6bf5ba958b66
stevelomax1 <steven.lomax1@gmail.com>
2024-07-06 20:19:17 -0400

    updated seed info

--- seeds.sql
+++ seeds.sql
@@ -2,16 +2,16 @@
 
 USE employee_db;
 
-INSERT INTO department (name) VALUES ('Engineering'), ('Sales'), ('Finance');
+INSERT INTO department (name) VALUES ('Engineering'), ('Management'), ('Finance');
 
 INSERT INTO role (title, salary, department_id) VALUES 
 ('Software Engineer', 80000, 1),
-('Sales Manager', 90000, 2),
+('Project Manager', 90000, 2),
 ('Accountant', 70000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, NULL),
-('Jane', 'Smith', 2, NULL),
-('Mike', 'Johnson', 3, NULL),
-('Alice', 'Brown', 1, 1),
-('Bob', 'Davis', 2, 2);
+('Steve', 'Lomax', 1, NULL),
+('Raven', 'Lomax', 2, NULL),
+('Paris', 'Lomax', 3, NULL),
+('Aleah', 'Lomax', 1, 1),
+('Miles', 'Lomax', 2, 2);
